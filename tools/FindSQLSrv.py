6658c2b98f642be0a4204831990a1126543a620f
lgandx <laurent.gaffie@gmail.com>
2021-03-25 23:06:48 -0300

    made compatible py2/py3

--- tools/FindSQLSrv.py
+++ tools/FindSQLSrv.py
@@ -16,12 +16,12 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from socket import *
 
-print('MSSQL Server Finder 0.2')
+print('MSSQL Server Finder 0.3')
 
 s = socket(AF_INET,SOCK_DGRAM)
 s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
-s.settimeout(2)
-s.sendto('\x02',('255.255.255.255',1434))
+s.settimeout(5)
+s.sendto(b'\x02',('255.255.255.255',1434))
 
 try:
     while 1:
@@ -31,7 +31,7 @@ try:
         else:
             print("===============================================================")
             print(("Host details: %s"%(address[0])))
-            print((data[2:]))
+            print((data[2:]).decode('latin-1'))
             print("===============================================================")
             print("")
 except:
